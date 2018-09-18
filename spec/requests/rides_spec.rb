694222f7f83f0de6358202d1b2a31eb9836521ed
Eno Bassey <ennyboy@gmail.com>
2018-09-19 00:02:15 +0100

    clean up pages

--- spec/requests/rides_spec.rb
+++ spec/requests/rides_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
+require 'shared_contexts'
 
 RSpec.describe "Rides", type: :request do
-  describe "GET /rides" do
-    it "works! (now write some real specs)" do
-      get rides_path
-      expect(response).to have_http_status(200)
-    end
-  end
+  let(:user1) { create(:user, password: 'password', password_confirmation: 'password') }
+  let(:user) { create(:user) }
+  let!(:ride) { create(:ride, user: user)}
+  let(:ride2) { create(:ride, user: user, start_location: 'ET', end_location: 'Yaba')}
+  let(:ride1) { create(:ride, user: user, start_location: 'VI', end_location: 'ET')}
+  # before do
+  #   sign_in(user1)
+  # end
+  # xdescribe 'GET /rides' do
+  #   it 'works! (now write some real specs)' do
+  #     get rides_path
+  #     expect(response).to have_http_status(200)
+  #   end
+  # end
 end
